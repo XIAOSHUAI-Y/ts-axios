f42f167b5880d4269f54ca43d1bd24f73cc4fd9d
XIAOSHUAI-Y <2138733925@qq.com>
2025-09-01 22:32:15 +0800

    处理响应data

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
 import { buildURL } from "./helpers/url"
 import { transformRequest } from "./helpers/data"
-import { AxiosPromise, AxiosRequestConfig } from "./types"
+import { AxiosPromise, AxiosRequestConfig, AxiosResponse } from "./types"
 import xhr from "./xhr"
 import { processHeaders } from "./helpers/headers"
 
+
 function axios(config: AxiosRequestConfig): AxiosPromise {
   processConfig(config)
-  return xhr(config)
+  return xhr(config).then((res) => {
+    return transformResponseData(res)
+  })
 }
 
 function processConfig(config: AxiosRequestConfig): void {
@@ -29,6 +32,11 @@ function transformHeaders (config: AxiosRequestConfig): any {
   // 从配置中解构出 headers 和 data，默认 headers 为空对象
   const {headers = {}, data} = config
   return processHeaders(headers, data)
+}
+
+function transformResponseData(res: AxiosResponse): AxiosResponse {
+  res.data = transformRequest(res.data)
+  return res
 }
 
 export default axios
